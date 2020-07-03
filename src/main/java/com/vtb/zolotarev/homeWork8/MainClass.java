2f87c9b6753314f516de66fdef94767dc2a2043c
arzek357 <arzek357@gmail.com>
2020-07-03 15:08:22 +0300

    Add HomeWork8

---
+++ src/main/java/com/vtb/zolotarev/homeWork8/MainClass.java
@@ -0,0 +1,44 @@
+package com.vtb.zolotarev.homeWork8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MainClass {
+
+    /*
+     * 1) Взять строку, состоящую из 100 слов разделенных пробелом, получить список слов длиннее 5 символов,
+     * и склеить их в одну строку с пробелом в качестве разделителя;
+     * 2) Найти список уникальных слов в двумерном массиве размером 5х5;
+     * 3) Посчитать сумму четных чисел в пределах от 100 до 200 (включительно);
+     * 4) Посчитать суммарную длину строк в одномерном массиве;
+     * 5) Из массива слов получить первые три слова в алфавитном порядке;
+     * */
+
+    //Task1
+    public static String getUniqueStringWithFixLength(String str, Predicate<String> pr) {
+        return Arrays.stream(str.split("\\s")).distinct().filter(pr).collect(Collectors.joining(" "));
+    }
+
+    //Task2
+    public static List<String> getUniqueWordsInArray(String[][] array) {
+        return Stream.of(array).flatMap(Arrays::stream).distinct().collect(Collectors.toList());
+    }
+
+    //Task3
+    public static int getSumEvenNumbers(int... numbers) {
+        return Arrays.stream(numbers).filter(s -> s % 2 == 0 && s <= 200 && s >= 100).sum();
+    }
+
+    //Task4
+    public static int getSumOfLengthStrings(String... strings) {
+        return Arrays.stream(strings).mapToInt(String::length).sum();
+    }
+
+    //Task5
+    public static List<String> getSortedWordsFixNumber(long number, String... strings) {
+        return Arrays.stream(strings).sorted().limit(number).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/com/vtb/zolotarev/homeWork8/TestAllTasks.java
@@ -0,0 +1,63 @@
+package com.vtb.zolotarev.homeWork8;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestAllTasks {
+
+    @Test
+    void task1Test() {
+        String testString = "Тщетно тщится щука ущемить леща " +
+                "Тщетно тщится щука ущемить леща " +
+                "Тщетно тщится щука ущемить леща " +
+                "Тщетно тщится щука ущемить леща " +
+                "Тщетно тщится щука ущемить леща " +
+                "Тщетно тщится щука ущемить леща " +
+                "Тщетно тщится щука ущемить леща " +
+                "Тщетно тщится щука ущемить леща " +
+                "Тщетно тщится щука ущемить леща " +
+                "Тщетно тщится щука ущемить леща";
+
+        String expectedString = "Тщетно тщится ущемить";
+        Assertions.assertEquals(expectedString, MainClass.getUniqueStringWithFixLength(testString, s -> s.length() > 5));
+    }
+
+    @Test
+    void task2Test() {
+        String[][] testArr = new String[5][5];
+
+        for (int i = 0; i < testArr.length; i++) {
+            Arrays.fill(testArr[i], String.format("Hi, %d", i));
+        }
+
+        testArr[0][0] = "Miss!";
+
+        List<String> expectedList = new ArrayList<>(Arrays.asList("Miss!","Hi, 0", "Hi, 1", "Hi, 2", "Hi, 3", "Hi, 4"));
+        Assertions.assertEquals(expectedList, MainClass.getUniqueWordsInArray(testArr));
+    }
+
+    @Test
+    void task3Test() {
+        int[] testArr = {3, 2, 88, 345, 200, 123, 102, 5, 9, 14};
+        int expectedResult = 302;
+        Assertions.assertEquals(expectedResult, MainClass.getSumEvenNumbers(testArr));
+    }
+
+    @Test
+    void task4Test() {
+        String[] testArr = {"a", "bb", "ccc", "dddd", "eeeee"};
+        int expectedResult = 15;
+        Assertions.assertEquals(expectedResult, MainClass.getSumOfLengthStrings(testArr));
+    }
+
+    @Test
+    void task5Test() {
+        String[] testArr = {"cc", "gggg", "aaa", "bbbbbbbbbbbbbbb", "p", "l"};
+        List<String> expectedList = new ArrayList<>(Arrays.asList("aaa", "bbbbbbbbbbbbbbb", "cc"));
+        Assertions.assertEquals(expectedList, MainClass.getSortedWordsFixNumber(3, testArr));
+    }
+}
