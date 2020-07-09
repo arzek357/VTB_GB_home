6d91b083cbe1f28ad0e51857415f48412538a792
arzek357 <arzek357@gmail.com>
2020-07-09 14:22:21 +0300

    Add HomeWork10

---
+++ src/main/java/com/vtb/zolotarev/homeWork10/stages/Tunnel.java
@@ -0,0 +1,34 @@
+package com.vtb.zolotarev.homeWork10.stages;
+
+
+import com.vtb.zolotarev.homeWork10.Car;
+
+import java.util.concurrent.Semaphore;
+
+public class Tunnel extends Stage {
+    private final Semaphore semaphore;
+    public Tunnel(Semaphore semaphore) {
+        this.semaphore = semaphore;
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void overcome(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                semaphore.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+                semaphore.release();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
